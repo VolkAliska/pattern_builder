2d5d581aec0852796087defb51f3c12c27900836
VolkAliska <viershtrassaliska@gmail.com>
2019-03-17 14:59:43 +0300

    initial commit

---
+++ Car.h
@@ -0,0 +1,20 @@
+/* Car.h */
+
+#ifndef CAR
+#define CAR
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+class Car{
+private:
+	//тип летательного устройства
+	string carType;
+	//хранилище информации об устройстве
+	map<string, string> parts;
+public:
+
+};
+
+#endif //CAR
